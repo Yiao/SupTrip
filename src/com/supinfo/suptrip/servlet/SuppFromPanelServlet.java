78ba739e2ec4af5066123e39d2f38c93d1a180af
yiao <yiao.sun@supinfo.com>
2016-03-23 16:58:51 +0100

    Ajoutation SupperServlet

---
+++ src/com/supinfo/suptrip/servlet/SuppFromPanelServlet.java
@@ -0,0 +1,34 @@
+package com.supinfo.suptrip.servlet;
+
+import com.supinfo.suptrip.dao.TripDAO;
+import com.supinfo.suptrip.dao.UserDAO;
+import com.supinfo.suptrip.entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by sya on 3/23/2016.
+ */
+@WebServlet(name = "SuppFromPanelServlet", urlPatterns = "/auth/suppFromPanel")
+public class SuppFromPanelServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TripDAO MyTripDAO = new TripDAO();
+
+        String tripId = request.getParameter("thisTrip");
+
+        MyTripDAO.suppTripFromUser(Long.valueOf(tripId));
+        response.sendRedirect("/auth/myTravels");
+    }
+}
